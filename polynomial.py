e9c9bc503713ebe3068e518d9bc5968c01b37ee5
HHF232 <59097602+HHF232@users.noreply.github.com>
2020-12-14 21:34:13 +0300

    Add files via upload

--- polynomial.py
+++ polynomial.py
@@ -1,323 +1,229 @@
-from copy import deepcopy
+class Polynomial:
 
-
-class Polynomial(object):
     def __init__(self, *coeff):
-        if isinstance(coeff[0], list):
-            listt = deepcopy(list(coeff[0]))
-            while listt[-1] == 0 and len(listt) >= 2:
-                listt = listt[:len(listt) - 1]
-            self.coeff = listt
-        elif isinstance(coeff[0], dict):
-            listt = []
-            dictt = deepcopy(dict(coeff[0]))
-            for i in range(max(dictt.keys()) + 1):
-                if i in dictt.keys():
-                    listt.append(dictt[i])
+        if isinstance(list(coeff)[0], dict):
+            a = []
+            d = list(coeff)[0]
+            for element in d:
+                a.append(element)
+            length = max(a)
+            self.coeffs = []
+            for i in range(length + 1):
+                if i in d:
+                    self.coeffs.append(d[i])
                 else:
-                    listt.append(0)
-            while listt[-1] == 0 and len(listt) >= 2:
-                listt = listt[:len(listt) - 1]
-            self.coeff = listt
-        elif isinstance(coeff[0], int):
-            listt = deepcopy(list(coeff))
-            while listt[-1] == 0 and len(listt) >= 2:
-                listt = listt[:len(listt) - 1]
-            self.coeff = listt
+                    self.coeffs.append(0)
+        elif isinstance(coeff[0], list):
+            self.coeffs = coeff[0]
         elif isinstance(coeff[0], Polynomial):
-            self.coeff = deepcopy(coeff[0].coeff)
+            self.coeffs = coeff[0].coeffs
         else:
-            raise BaseException(self)
+            self.coeffs = list(coeff)
+        j = len(self.coeffs) - 1
+        while self.coeffs[j] == 0 and j > 0:
+            j -= 1
+        if j == 0:
+            ans = self.coeffs[0]
+            self.coeffs = []
+            self.coeffs.append(ans)
+        else:
+            self.coeffs = self.coeffs[:j + 1]
 
     def __repr__(self):
-        coeff = self.coeff
-        while coeff[-1] == 0:
-            coeff = coeff[:len(coeff) - 1]
-        ret = "Polynomial ["
-        for i in range(len(coeff)):
-            ret += str(coeff[i]) + ", "
-        ret = ret[:len(ret) - 2] + "]"
-        return ret
+        return "Polynomial " + str(self.coeffs)
 
     def __str__(self):
-        poly = ""
-        for i in range(len(self.coeff)):
-            if self.coeff[i] != 0:
-                if i == 0:
-                    poly += str(self.coeff[i])
-                elif i == 1:
-                    if self.coeff[i] != 1 and self.coeff[i] != -1:
-                        poly = str(self.coeff[i]) + "x" + " + " + poly
-                    elif self.coeff[i] == 1:
-                        poly = "x" + " + " + poly
-                    else:
-                        poly = "-x" + " + " + poly
-                elif self.coeff[i] != 1 and self.coeff[i] != -1:
-                    poly = str(self.coeff[i]) + "x^" + str(i) + " + " + poly
-                elif self.coeff[i] == -1:
-                    poly = "-x^" + str(i) + " + " + poly
-                else:
-                    poly = "x^" + str(i) + " + " + poly
-        if self.coeff[0] == 0:
-            poly = poly[:len(poly) - 3]
-        for i in range(len(poly)):
-            if poly[i: i + 3] == "+ -":
-                poly = poly[:i] + "- " + poly[i + 3:]
-        if poly == "":
-            return "0"
-        else:
-            return poly
-
-    def degree(self):
-        return len(self.coeff) - 1
-
-    def __add__(self, other):
-        if isinstance(other, Polynomial) and isinstance(self, Polynomial):
-            answ = []
-            if len(self.coeff) >= len(other.coeff):
-                list2 = other.coeff
-                for i in range(len(self.coeff) - len(other.coeff)):
-                    list2.append(0)
-                for i in range(len(self.coeff)):
-                    answ.append(self.coeff[i] + list2[i])
+        ans = ""
+        for i in range(1, len(self.coeffs) + 1):
+            if self.coeffs[-i] == 0:
+                continue
+            elif self.coeffs[-i] > 0:
+                ans += " + "
             else:
-                list2 = self.coeff
-                for i in range(len(other.coeff) - len(self.coeff)):
-                    list2.append(0)
-                for i in range(len(list2)):
-                    answ.append(list2[i] + other.coeff[i])
-            return Polynomial(answ)
-        elif isinstance(other, int) and isinstance(self, Polynomial):
-            answ = self.coeff
-            answ[0] = answ[0] + other
-            return Polynomial(answ)
-        else:
-            raise BaseException(self)
-
-    def __radd__(self, other):
-        poly1 = deepcopy(self)
-        poly2 = deepcopy(other)
-        if isinstance(other, Polynomial) and isinstance(self, Polynomial):
-            return poly1 + poly2
-        elif isinstance(other, int) and isinstance(self, Polynomial):
-            return poly1 + poly2
-        else:
-            raise BaseException(self)
-
-    def __sub__(self, other):
-        if isinstance(other, Polynomial):
-            answ = []
-            if len(self.coeff) >= len(other.coeff):
-                for i in range(len(other.coeff)):
-                    answ.append(self.coeff[i] - other.coeff[i])
-                answ += self.coeff[len(other.coeff):]
-            else:
-                for i in range(len(self.coeff)):
-                    answ.append(self.coeff[i] - other.coeff[i])
-                answ += list(map(lambda x: -x, other.coeff[len(other.coeff):]))
-            return Polynomial(answ)
-        elif isinstance(other, int):
-            answ = deepcopy(self.coeff)
-            answ[0] = answ[0] - other
-            return Polynomial(answ)
-        else:
-            raise BaseException(self)
-
-    def __rsub__(self, other):
-        poly1 = deepcopy(self)
-        poly2 = deepcopy(other)
-        if isinstance(other, Polynomial):
-            return poly2 - poly1
-        elif isinstance(other, int):
-            ans = deepcopy(self.coeff)
-            for i in range(len(ans)):
-                ans[i] = (-1) * ans[i]
-            return Polynomial(ans) + other
-        else:
-            raise BaseException(self)
+                ans += " - "
+            if abs(self.coeffs[-i]) != 1 or len(self.coeffs) - i == 0:
+                ans += str(abs(self.coeffs[-i]))
+            if len(self.coeffs) - i > 0:
+                ans += "x"
+            if len(self.coeffs) - i > 1:
+                ans += "^" + str(len(self.coeffs) - i)
+        if ans[1] == "+":
+            ans = ans[3:]
+        elif ans[1] == "-":
+            ans = "-" + ans[3:]
+        return ans
 
     def __eq__(self, other):
-        if isinstance(other, Polynomial):
-            if len(self.coeff) != len(other.coeff):
-                return False
+        if isinstance(other, int):
+            if len(self.coeffs) == 1 and self.coeffs[0] == other:
+                return True
             else:
-                for i in range(len(self.coeff)):
-                    if self.coeff[i] != other.coeff[i]:
-                        return False
-            return True
+                return False
+        elif isinstance(other, Polynomial):
+            if self.coeffs == other.coeffs:
+                return True
+            else:
+                return False
         else:
-            raise BaseException(self, other)
+            return False
+
+    def __add__(self, other):
+        ans = Polynomial([element for element in self.coeffs])
+        if isinstance(other, (float, int)):
+            ans.coeffs[0] += other
+        elif isinstance(other, Polynomial):
+            l0 = len(self.coeffs)
+            l1 = len(other.coeffs)
+            length = min(l0, l1)
+            for i in range(length):
+                ans.coeffs[i] += other.coeffs[i]
+            if l0 < l1:
+                for i in range(l0, l1):
+                    ans.coeffs.append(other.coeffs[i])
+        return Polynomial(ans.coeffs)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __neg__(self):
+        new = Polynomial(self.coeffs)
+        for i in range(len(new.coeffs)):
+            new.coeffs[i] = -new.coeffs[i]
+        return new
+
+    def __pos__(self):
+        return Polynomial(self.coeffs)
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
 
     def __call__(self, x):
-        if isinstance(x, float) or isinstance(x, int):
-            summ = self.coeff[0]
-            for i in range(1, len(self.coeff)):
-                summ += self.coeff[i] * (x ** i)
-            return summ
-        else:
-            raise BaseException(self)
+        result = 0
+        length = len(self.coeffs)
+        for i in range(length):
+            result += self.coeffs[i] * (x ** i)
+        return result
+
+    def degree(self):
+        return len(self.coeffs) - 1
+
+    def der(self, d=1):
+        if d > self.degree():
+            return Polynomial(0)
+        new = Polynomial(self)
+        for k in range(d):
+            for i in range(len(new.coeffs)):
+                new.coeffs[i] *= i
+            new.coeffs = new.coeffs[1:]
+        if len(new.coeffs) == 0:
+            return Polynomial([0])
+        return new
 
     def __mul__(self, other):
-        if isinstance(other, int):
-            res = []
-            for i in range(len(self.coeff)):
-                res.append(self.coeff[i] * other)
-            return Polynomial(res)
-        elif isinstance(other, Polynomial):
-            res = [self.coeff[0] * other.coeff[0]]
-            for i in range(1, self.degree() + other.degree() + 1):
-                coeff_res = 0
-                for j in range(max(0, i - other.degree()), min(i, self.degree()) + 1):
-                    coeff_res += self.coeff[j] * other.coeff[i - j]
-                res.append(coeff_res)
-            return Polynomial(res)
-        else:
-            raise BaseException(self, other)
+        l0 = len(self.coeffs)
+        if isinstance(other, Polynomial):
+            l1 = len(other.coeffs)
+            ans = [0 for i in range(l0 + l1)]
+            for i in range(l0):
+                for j in range(l1):
+                    ans[i + j] += self.coeffs[i] * other.coeffs[j]
+            return Polynomial(ans)
+        elif isinstance(other, (float, int)):
+            ans = []
+            for i in range(l0):
+                ans.append(self.coeffs[i] * other)
+            return Polynomial(ans)
 
     def __rmul__(self, other):
-        return Polynomial(other) * Polynomial(self)
-
-    def __mod__(self, other):
-        if isinstance(other, int):
-            ans = []
-            for i in range(len(self.coeff)):
-                ans.append(self.coeff[i] % other)
-
-            return Polynomial(ans)
-        elif isinstance(other, Polynomial):
-            if self.degree() < other.degree():
-                raise BaseException(self, other)
-            else:
-                l1 = list(self.coeff)
-                l2 = list(other.coeff)
-                l1.reverse()
-                l2.reverse()
-                k = l1[0] / l2[0]
-                for i in range(len(l2)):
-                    l1[i] -= (l2[i] * k)
-                l1 = l1[1:]
-                while len(l1) >= len(l2):
-                    k = l1[0] / l2[0]
-                    for i in range(len(l2)):
-                        l1[i] -= (l2[i] * k)
-                    l1 = l1[1:]
-                l1.reverse()
-                if len(l1) == 0 or set(l1) == {0}:
-                    return Polynomial([0])
-                else:
-                    return Polynomial(l1)
-        else:
-            raise BaseException(self, other)
-
-    def gcd(self, other):
-        if isinstance(other, Polynomial):
-            m1 = deepcopy(self)
-            m2 = deepcopy(other)
-            if m1.degree() < m2.degree():
-                m3 = m1
-                m1 = m2
-                m2 = m3
-            while str(m1 % m2) != "0":
-                m3 = m2
-                m2 = m1 % m2
-                m1 = m3
-            return m2
-        else:
-            raise BaseException(self, other)
+        return self * other
 
     def __iter__(self):
-        self.n = 0
-        return self
+        ans = []
+        i = 0
+        while i < len(self.coeffs):
+            if self.coeffs[i] != 0:
+                ans.append((i, self.coeffs[i]))
+            i += 1
+        return iter(ans)
 
     def __next__(self):
-        if self.n <= self.degree():
-            monome = (self.n, self.coeff[self.n])
-            self.n += 1
-            return monome
-        else:
-            raise StopIteration
+        return next(self)
 
-    def der(self, *num):
-        if isinstance(num[0], int) and len(num) == 1 :
-            listt = deepcopy(self.coeff)
-            j = 1
-            while j <= num[0]:
-                for i in range(len(listt)):
-                    listt[i] *= i
-                if len(listt) > 1:
-                    listt = listt[1:]
-                j += 1
-            return Polynomial(listt)
-        elif len(num) == 0:
-            listt = deepcopy(self.coeff)
-            for i in range(len(listt)):
-                listt[i] *= i
-            if len(listt) > 1:
-                listt = listt[1:]
-            return Polynomial(listt)
+class NotOddDegreeException(Exception):
+    def __init__(self):
+        pass
+
+class DegreeIsTooBigException(Exception):
+    def __init__(self):
+        pass
+
+class RealPolynomial(Polynomial):
+    def __init__(self, *coefficients):
+        if isinstance(coefficients[0], (Polynomial, list, dict)):
+            super().__init__(coefficients[0])
         else:
-            raise BaseException(self)
+            super().__init__(coefficients)
+        if self.degree() % 2 != 1:
+            raise NotOddDegreeException
+
+    def find_root(self):
+        s = -1
+        e = 1
+        while (self(s) > 0 and self(e) > 0) or (self(s) < 0 and self(e) < 0):
+            s *= 2
+            e *= 2
+        if self(s) == 0:
+            return s
+        if self(e) == 0:
+            return e
+        vs = self(s)
+        ve = self(e)
+        while abs(ve - vs) >= 1e-6:
+            m = (s + e) / 2
+            vm = self(m)
+            if vs == 0:
+                return s
+            if ve == 0:
+                return e
+            if vm == 0:
+                return m
+            if (vs < 0 and vm > 0) or (vs > 0 and vm < 0):
+                e = m
+            if (ve < 0 and vm > 0) or (ve > 0 and vm < 0):
+                s = m
+            vs = self(s)
+            ve = self(e)
+        return (e + s) / 2
 
 
 class QuadraticPolynomial(Polynomial):
-    def __init__(self, coeff):
-        if isinstance(coeff, list) and len(coeff) <= 3:
-            super().__init__(coeff)
+    def __init__(self, *coeff):
+        if isinstance(coeff[0], (Polynomial, list, dict)):
+            super().__init__(coeff[0])
         else:
-            raise BaseException(self)
+            super().__init__(coeff)
+        if self.degree() > 2:
+            raise DegreeIsTooBigException
 
     def solve(self):
-        if len(self.coeff) == 3:
-            discr = self.coeff[1] ** 2 - 4 * self.coeff[0] * self.coeff[2]
-            if discr > 0:
-                x1 = (-self.coeff[1] + discr ** (1 / 2)) / (2 * self.coeff[2])
-                x2 = (-self.coeff[1] - discr ** (1 / 2)) / (2 * self.coeff[2])
-                return [min(x1, x2), max(x1, x2)]
-            elif discr == 0:
-                x1 = -self.coeff[1] / (2 * self.coeff[2])
-                return [x1]
+        if len(self.coeffs) == 3:
+            a, b, c = self.coeffs[2], self.coeffs[1], self.coeffs[0]
+            disc = b ** 2 - (4 * a * c)
+            if disc > 0:
+                x1 = (-b + disc ** (1 / 2)) / (2 * a)
+                x2 = (-b - disc ** (1 / 2)) / (2 * a)
+                return [x1, x2]
+            elif disc == 0:
+                x = -b / (2 * a)
+                return [x]
             else:
                 return []
-        elif len(self.coeff) == 2:
-            if self.coeff[1] != 0:
-                x1 = -self.coeff[0] / self.coeff[1]
-                return [x1]
-            elif self.coeff[0] == 0:
-                return "All"
-            else:
-                return []
+        elif len(self.coeffs) == 2:
+            b, c = self.coeffs[1], self.coeffs[0]
+            x = -c / b
+            return [x]
         else:
-            if self.coeff[0] == 0:
-                return "All"
-            else:
-                return []
-
-
-class RealPolynomial(Polynomial):
-    def __init__(self, coeff):
-        if isinstance(coeff, list):
-            polly = Polynomial(coeff)
-            if polly.degree() % 2 == 1:
-                super().__init__(coeff)
-            else:
-                raise BaseException(self)
-        else:
-            raise BaseException(self)
-
-    def find_root(self):
-        nach = -64
-        con = 64
-        while (Polynomial(self.coeff)(nach) >= 0 and Polynomial(self.coeff)(con) >= 0) or (Polynomial(self.coeff)(nach) <= 0 and Polynomial(self.coeff)(con) <= 0):
-            nach = nach * 2
-            con = con * 2
-        while con - nach >= 2 * (-17):
-            mid1 = nach + ((con - nach) / 3)
-            mid2 = 2 * mid1 - nach
-            if (Polynomial(self.coeff)(nach) >= 0 and Polynomial(self.coeff)(mid1) <= 0) or (Polynomial(self.coeff)(nach) <= 0 and Polynomial(self.coeff)(mid1) >= 0):
-                con = mid1
-            elif (Polynomial(self.coeff)(mid2) >= 0 and Polynomial(self.coeff)(mid1) <= 0) or (Polynomial(self.coeff)(mid2) <= 0 and Polynomial(self.coeff)(mid1) >= 0):
-                con = mid2
-                nach = mid2
-            else:
-                nach = mid2
-        return nach
+            return []
